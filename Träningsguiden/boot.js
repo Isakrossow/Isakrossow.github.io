7b8f2310fe1557a5d9caf701d708f697670da371
Isakrossow <43953882+Isakrossow@users.noreply.github.com>
2019-05-16 18:44:59 +0200

    k

--- Träningsguiden/boot.js
+++ Träningsguiden/boot.js
@@ -43,13 +43,13 @@ $(document).ready(function(){
     }, 1000);
   });
 
-  $("nav").mouseenter(function(){
-    $("nav").css({opacity:0.7});
+  $("nav").mouseleave(function(){
+    $("nav").css({opacity:1});
 
   });
 
-    $("nav").mouseleave(function(){
-      $("nav").css({opacity:1});
+    $("nav").mouseenter(function(){
+      $("nav").css({opacity:0.5});
 
     });
 
